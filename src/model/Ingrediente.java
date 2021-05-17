7bd4c1bc030500a111476b78cea847aac41fb379
wild.chamo <joselitobedoya230@gmail.com>
2021-05-17 17:07:20 -0500

    creacion del modelo de ingrediente. Creé el constructor

--- src/model/Ingrediente.java
+++ src/model/Ingrediente.java
@@ -5,11 +5,14 @@
  */
 package model;
 
+import com.mongodb.BasicDBObject;
+import java.util.HashSet;
+import java.util.Set;
 /**
  *
  * @author wild.chamo
  */
-public class Ingrediente {
+public class Ingrediente extends BasicDBObject {
 
     private final String NOMBRE = "nombreIngrediente";
     private final String CANTIDAD = "cantidadIngrediente";
@@ -18,5 +21,36 @@ public class Ingrediente {
 
     public Ingrediente() {
         partial = false;
+    }
+
+    public Ingrediente(String nombreIngrediente, String cantidadIngrediente,  String medidaIngrediente) {
+        //clave y valor
+        this.put(NOMBRE, nombreIngrediente);
+        this.put(CANTIDAD, cantidadIngrediente);
+        this.put(MEDIDA, medidaIngrediente);
+
+        this.markAsPartialObject();
+    }
+
+    
+    @Override
+    public void markAsPartialObject() {
+        Set<String> set = keySet();
+        set.remove("_id");
+
+        Set<String> setThis = new HashSet<String>();
+        setThis.add(NOMBRE);
+        setThis.add(CANTIDAD);
+        setThis.add(MEDIDA);
+
+        partial = !set.equals(setThis);
+    }
+
+    public boolean isPartialObject() {
+        return partial;
+    }
+
+    public void setPartial(boolean partial) {
+        this.partial = partial;
     }
 }
