0f413c051d43a3f8923b8299c29d7c8db8610f8c
wild.chamo <joselitobedoya230@gmail.com>
2021-05-19 14:38:46 -0500

    intento de conexion para traer los platos

--- src/tallernosql2021/PlatoIngrediente.java
+++ src/tallernosql2021/PlatoIngrediente.java
@@ -15,7 +15,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.LinkedList;
 
-
 /**
  *
  * @author David
@@ -26,25 +25,36 @@ public class PlatoIngrediente {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Ingrediente obji = new Ingrediente("nombregenerico",1.1,"miligramos");
+        /*  Ingrediente obji = new Ingrediente("nombregenerico",1.1,"miligramos");
         LinkedList<Ingrediente> ingredientes = new LinkedList<Ingrediente>();
         ingredientes.add(obji);
         ingredientes.add(obji);
         ingredientes.add(obji);
         Plato objp = new Plato("nombreplato", 15, 300, 400,"de la abuela",ingredientes );
-
+         */        
         BaseDatos objbd = new BaseDatos();
+        Plato objpp=new Plato();
+        LinkedList<Plato> listaP;
         
         try {
-            DB objb= objbd.createConnection();
+            DB objb = objbd.createConnection();
             if (objb != null) {
-                DBCollection collection= objb.getCollection(objp.getClass().getSimpleName());
-                collection.insert(objp);
+//                DBCollection collection= objb.getCollection(objp.getClass().getSimpleName());
+//                collection.insert(objp);
+
+                listaP = (LinkedList<Plato>)objbd.findAll(objpp.getClass());
+                
+                for(int i=0; i<listaP.size();i++){
+                    Plato get = listaP.get(i);
+                    System.out.println("datos " + get.toJson()+"\n");
+                }
+                
+                
             }
         } catch (UnknownHostException ex) {
             Logger.getLogger(TallerNoSQL2021.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
     }
-
+    
 }
